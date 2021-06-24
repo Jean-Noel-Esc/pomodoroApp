6408cb5a7e39e2f16bc4777577284728d0f3713e
Jean-Noel-Esc <60701865+Jean-Noel-Esc@users.noreply.github.com>
2021-06-24 17:01:41 +0200

    gestion  timer travail pause

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const btnPause = document.querySelector('.b2');
 const btnreset = document.querySelector('.b3');
 const cycles = document.querySelector('h2');
 
-let tempsInitial = 1800;
-let tempsDeRepos = 300;
+let tempsInitial = 3;
+let tempsDeRepos = 3;
 let pause = false;
 let nbDeCycles = 0;
 cycles.innerText = `Nombre de cycles ${ nbDeCycles }`;
@@ -17,3 +17,33 @@ affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${
 affichageRepos.innerText = `${Math.trunc(tempsDeRepos/60)} : ${
         (tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
 
+
+btnGO.addEventListener('click', () => {
+    tempsInitial --;
+    affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${
+        (tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial %
+        60}`;
+
+        let timer = setInterval(() => {
+            if(tempsInitial > 0){
+                tempsInitial --;
+                affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${
+                    (tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial %
+                    60}`;
+            }
+            else if (tempsDeRepos === 0 && tempsInitial === 0){
+                tempsInitial = 3;
+                tempsDeRepos = 3;
+                nbDeCycles++;
+                cycles.innerText = `Nombres de cycles ${nbDeCycles}`;
+                affichageTravail.innerText = `${Math.trunc(tempsInitial/60)} : ${(tempsInitial % 60 < 10) ? `0${tempsInitial % 60}` : tempsInitial % 60}`;
+                affichageRepos.innerText = `${Math.trunc(tempsDeRepos/60)} : ${(tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+            }
+            else if (tempsInitial === 0){
+                tempsDeRepos--;
+                affichageRepos.innerText = `${Math.trunc(tempsDeRepos/60)} : ${
+                    (tempsDeRepos % 60 < 10) ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60}`;
+            }
+
+        }, 1000);
+})     
